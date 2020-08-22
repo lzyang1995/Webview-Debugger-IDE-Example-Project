e07633cd7c71e2e3e203829ecadd541f82650ebd
lzyang1995 <lzyang1995@outlook.com>
2020-08-22 11:27:14 +0800

    add configuration files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function executeProtocol(href) {
+  let iframe = document.createElement("iframe");
+  iframe.style.width = "0px";
+  iframe.style.height = "0px";
+  iframe.hidden = true;
+
+  iframe.src = href;
+  (document.body || document.documentElement).append(iframe);
+
+  setTimeout(() => {
+    iframe && iframe.parentNode && iframe.remove()
+  }, 1000);
+}
+
+function setTitle(title) {
+  if (title) {
+    let href = "abc://web/settitle?title=" + encodeURIComponent(title);
+    executeProtocol(href);
+  }
+}
+
+function getContact(callback) {
+  if (callback) {
+    let href = "abc://web/info/getContact?callback=" + encodeURIComponent("javascript:" + callback);
+    executeProtocol(href);
+  }
+}
+
+function share(val) {
+  if (val) {
+    let href = "abc://web/share?share=" + encodeURIComponent(val);
+    executeProtocol(href);
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Request: abc://web/settitle?title=Good Title</p>
+      <button onClick={() => setTitle("Good Title")}>set title</button>
+      <hr />
+
+      <p>Request: abc://web/info/getContact?callback=javascript:getContactCallback</p>
+      <button onClick={() => getContact("getContactCallback")}>get contact</button>
+      <hr />
+
+      <p>Request: abc://web/share?share=good share</p>
+      <p>(This does not exist in default protocol configuration)</p>
+      <button onClick={() => share("good share")}>share</button>
+      <hr />
     </div>
   );
 }
